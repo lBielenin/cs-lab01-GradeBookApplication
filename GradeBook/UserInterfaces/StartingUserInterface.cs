83bdabccec48d93673e3cbbdc68a38d5a9fb9685
Łukasz Bielenin <Lukasz.Bielenin@microsoft.wsei.edu.pl>
2022-03-06 14:30:31 +0100

    Add type of graqdebook creation switch

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -34,15 +34,32 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 2)
+            if (parts.Length != 3)
             {
-                Console.WriteLine("Command not valid, Create requires a name.");
+                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
             var name = parts[1];
+            var gradeBookType = parts[2];
+
             BaseGradeBook gradeBook = new BaseGradeBook(name);
             Console.WriteLine("Created gradebook {0}.", name);
             GradeBookUserInterface.CommandLoop(gradeBook);
+        }
+
+        private BaseGradeBook CreateBookByInput(string name, string input)
+        {
+            switch(input)
+            {
+
+            }
+            return input switch
+            {
+            "standard" => new StandardGradeBook(name),
+            "ranked" => new RankedGradeBook(name),
+            _ => return;
+}
+    }
         }
 
         public static void LoadCommand(string command)
