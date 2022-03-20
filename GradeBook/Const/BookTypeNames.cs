7bd6802c7225b5b540f17dd22c0007a0925f93f1
Łukasz Bielenin <Lukasz.Bielenin@microsoft.wsei.edu.pl>
2022-03-20 14:20:20 +0100

    Add book type names constant for better readability

---
+++ GradeBook/Const/BookTypeNames.cs
@@ -0,0 +1,8 @@
+﻿namespace GradeBook.Const
+{
+    public static class BookTypeNames
+    {
+        public const string RANKED = "ranked";
+        public const string STANDARD = "standard";
+    }
+}
