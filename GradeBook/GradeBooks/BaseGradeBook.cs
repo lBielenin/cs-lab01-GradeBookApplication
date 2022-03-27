6501b3a0c6b4cd05b4462e2c08bf6cef5d7fdb4c
lbielenin <lbielenin@prescientco.com>
2022-03-27 12:23:33 +0200

    Fixed typo in IsWeighted prop of BaseGradeBook class

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -15,13 +15,13 @@ namespace GradeBook.GradeBooks
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
         protected static List<char> Grades = new List<char> { 'A', 'B', 'C', 'D', 'F' };
-        public bool IsWeight { get; set; }
+        public bool IsWeighted { get; set; }
 
         public BaseGradeBook(string name, bool isWeight)
         {
             Name = name;
             Students = new List<Student>();
-            IsWeight  = isWeight;
+            IsWeighted  = isWeight;
         }
         public void AddStudent(Student student)
         {
